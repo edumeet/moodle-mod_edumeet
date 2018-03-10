4eb3a170df73e6c7c91bffe91b55e8a3f3eb024f
Mészáros Mihály <misi@niif.hu>
2018-03-10 08:50:23 +0100

    version ++

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_knockplop';
-$plugin->release = '1.0.0';
-$plugin->version = 2017101906;
+$plugin->release = '1.0.1';
+$plugin->version = 2018031001;
 $plugin->requires = 2015111600;
 $plugin->maturity = MATURITY_STABLE;
