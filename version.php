25bb41e740da56adb5fd39480556af8455f1463c
Mészáros Mihály <misi@majd.eu>
2019-04-03 13:08:10 +0200

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_multipartymeeting';
-$plugin->release = '1.0.1';
-$plugin->version = 2018031001;
+$plugin->release = '1.0.0';
+$plugin->version = 2019040301;
 $plugin->requires = 2015111600;
 $plugin->maturity = MATURITY_STABLE;
